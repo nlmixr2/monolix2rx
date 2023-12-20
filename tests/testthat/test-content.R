e95f92325d629652f73a9a8b9591fa966f8f47ea
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-12-20 15:51:49 -0600

    Start content test

---
+++ tests/testthat/test-content.R
@@ -0,0 +1,13 @@
+.content("ID = {use=identifier}
+TIME = {use=time}
+EVID = {use=eventidentifier}
+AMT = {use=amount}
+DV = {use=observation, name={y1, y2}, yname={'1', '2'}, type={continuous, continuous}}
+ADM = {use=administration}
+YTYPE = {use=observationtype}
+WT={use=covariate, type=continuous}
+CRCL={use=covariate, type=continuous}
+E0 = {use = regressor}
+Emax = {use = regressor}
+Race = {type=categorical, categories={Caucasian, Black, Latin}}
+Sex = {type=categorical, categories={M, F}}")
