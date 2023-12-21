9b0fd98255e4c44a7f2681f8ce099ab590b15727
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-12-20 21:06:19 -0600

    Add tests for longitudinal

---
+++ tests/testthat/test-longitudinal.R
@@ -0,0 +1,10 @@
+test_that("longitudinal()", {
+
+  f <- .longitudinal("input={pkadd__err, prop__err, pdadd__err}
+file='pk.turnover.emax3-monolix.txt'")
+
+  expect_equal(f$input, c("pkadd__err", "prop__err", "pdadd__err"))
+  expect_equal(f$file, "pk.turnover.emax3-monolix.txt")
+  expect_true(inherits(f, "monolix2rxLongitudinal"))
+
+})
