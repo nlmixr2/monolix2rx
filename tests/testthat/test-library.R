9d395407fe4cbd683615fbecff8022ae588c28a8
Matthew L. Fidler <matthew.fidler@gmail.com>
2024-01-14 14:40:27 -0600

    Add library tests

---
+++ tests/testthat/test-library.R
@@ -0,0 +1,50 @@
+libDir <- getOption("monolix2rx.library", NULL)
+.env <- new.env(parent=emptyenv())
+.env$txt <- NULL
+libDir <- sub("[/\\]+$", "", libDir)
+getMlxTxt <- function(dd=libDir, env=.env) {
+  .files <- list.files(dd)
+  for (.f in .files) {
+    .next <- file.path(dd, .f)
+    if (dir.exists(.next)) {
+      getMlxTxt(dd=.next, env=env)
+    }
+    .nc <- nchar(.f)
+    if (.nc < 4) next
+    if (substr(.f, .nc-3, .nc) == ".txt") {
+      env$txt <- c(env$txt, .next)
+    }
+  }
+}
+
+getMlxTxt()
+txt <- .env$txt
+
+unparsed <- function(x, ...) {
+  .p <- .unparsedMlxtran(x, ...)
+  .p <- .p[(.p %in% c("$MONOLIX$COMMENTS$COMMENTS", "$DATA_FORMATTING$OPERATIONS$TREATMENTS",
+                      "$DATA_FORMATTING$OPERATIONS$OBSERVATIONS", "$DATA_FORMATTING$SETTINGS$LINES",
+                      "$DATA_FORMATTING$SETTINGS$LIES"))]
+}
+
+for (f2 in txt) {
+  if (file.exists(f2)) {
+    m <- try(mlxTxt(f2))
+    test_that(paste("mlxtran without equation", f2), {
+      expect_true(inherits(m, "monolix2rxMlxtran"))
+    })
+    if (requireNamespace("rxode2", quietly = TRUE)) {
+      m2 <- try(monolix2rx(m), silent=TRUE)
+      if (inherits(m2, "try-error")) {
+        msg <- attr(m2, "condition")$message
+        test_that(paste0("error is known: ", msg, "\n", f2),{
+          expect_true(grepl("(count|categorical|event|equations to translate)", msg))
+        })
+      } else {
+        test_that(paste0("monolix2rx: ", f2), {
+          expect_true(inherits(m2, "rxUi"))
+        })
+      }
+    }
+  }
+}
