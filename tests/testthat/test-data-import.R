ce31210cb9bf4aa23dee0153cf74f01517d1e2bb
Matthew L. Fidler <matthew.fidler@gmail.com>
2024-05-01 18:19:44 -0500

    Move vdiffr to a suggested package

--- tests/testthat/test-data-import.R
+++ tests/testthat/test-data-import.R
@@ -5,21 +5,25 @@ test_that("single endpoint import", {
 
   rx <- monolix2rx(file.path(pkgTheo, "theophylline_project.mlxtran"))
   expect_true(inherits(rx, "rxUi"))
-  vdiffr::expect_doppelganger("single-endpoint-theo",
-                              plot(rx))
-  vdiffr::expect_doppelganger("single-endpoint-theo-p1",
-                              plot(rx, page=1))
-  vdiffr::expect_doppelganger("single-endpoint-theo-pall",
-                              plot(rx, page=TRUE))
+  if (requireNamespace("vdiffr", quietly=TRUE)) {
+    vdiffr::expect_doppelganger("single-endpoint-theo",
+                                plot(rx))
+    vdiffr::expect_doppelganger("single-endpoint-theo-p1",
+                                plot(rx, page=1))
+    vdiffr::expect_doppelganger("single-endpoint-theo-pall",
+                                plot(rx, page=TRUE))
+  }
 })
 
 test_that("multiple endpoint import", {
   rx <- monolix2rx(file.path(pkgTheo, "parent_metabolite_project.mlxtran"))
   expect_true(inherits(rx, "rxUi"))
-  vdiffr::expect_doppelganger("multiple-endpoint-theo",
-                              plot(rx))
-  vdiffr::expect_doppelganger("multiple-endpoint-theo-p1",
-                              plot(rx, page=1))
-  vdiffr::expect_doppelganger("multiple-endpoint-theo-pall",
-                              plot(rx, page=TRUE))
+  if (requireNamespace("vdiffr", quietly=TRUE)) {
+    vdiffr::expect_doppelganger("multiple-endpoint-theo",
+                                plot(rx))
+    vdiffr::expect_doppelganger("multiple-endpoint-theo-p1",
+                                plot(rx, page=1))
+    vdiffr::expect_doppelganger("multiple-endpoint-theo-pall",
+                                plot(rx, page=TRUE))
+  }
 })
