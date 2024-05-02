5c4799087ea2e5629a9e6f783e756637b6cd5028
Matthew L. Fidler <matthew.fidler@gmail.com>
2024-05-02 14:44:27 -0500

    Add rxSolve method for monolix2rx objects

--- R/buildParser.R
+++ R/buildParser.R
@@ -102,7 +102,157 @@
                     grammar_ident="dataSettings")
   file.rename(devtools::package_file("src/dataSettings.g.d_parser.c"),
               devtools::package_file("src/dataSettings.g.d_parser.h"))
-
+  .monolix2rxBuildRxSolve()
   invisible("")
 }
+
+.monolix2rxBuildRxSolve <- function() {
+  message("build options for rxSolve to match NONMEM")
+  .args <- deparse(eval(str2lang(paste0("args(rxode2::rxSolve)"))))
+  .args[1] <- paste0("rxSolve.monolix2rx <-", .args[1])
+  .args <- .args[-length(.args)]
+  .extra <- quote({
+    if (missing(cores)) {
+      cores <- 0L
+    }
+    if (missing(covsInterpolation)) {
+      covsInterpolation <- "locf"
+      .minfo("using locf interpolation like Monolix, specify directly to change")
+    }
+    if (!missing(nStud)) {
+      if (missing(dfSub)) {
+        if (!is.null(object$meta$dfSub)) {
+          dfSub <- object$meta$dfSub
+          .minfo(paste0("using dfSub=", dfSub, " from Monolix"))
+        } else if (!is.null(object$dfSub)) {
+          dfSub <- object$dfSub
+          .minfo(paste0("using dfSub=", dfSub, " from Monolix"))
+        }
+      }
+      if (missing(dfObs)) {
+        if (!is.null(object$meta$dfObs)) {
+          dfObs <- object$meta$dfObs
+          .minfo(paste0("using dfObs=", dfObs, " from Monolix"))
+        } else if (!is.null(object$dfObs)) {
+          dfObs <- object$dfObs
+          dfObs <- object$meta$dfObs
+          .minfo(paste0("using dfObs=", dfObs, " from Monolix"))
+        }
+      }
+      if (missing(thetaMat)) {
+        if (!is.null(object$meta$thetaMat)) {
+          thetaMat <- object$meta$thetaMat
+          .minfo(paste0("using thetaMat from Monolix"))
+        } else if (!is.null(object$thetaMat)) {
+          thetaMat <- object$meta$thetaMat
+          .minfo(paste0("using thetaMat from Monolix"))
+        }
+      }
+    }
+    # The theta/omega comes from the ui
+    if ((missing(events) && missing(params))) {
+      if (!is.null(object$monolixData)) {
+        events <- object$monolixData
+        .minfo(paste0("using Monolix's data for solving"))
+      }
+    }
+    .atol <- .rtol <- .getRtolAtol(object)
+    if (missing(atol)) {
+      atol <- .atol
+      .minfo(paste0("using Monolix specified atol=", atol))
+    }
+    if (missing(rtol)) {
+        rtol <- .rtol
+        .minfo(paste0("using Monolix specified rtol=", rtol))
+    }
+    # ssAtol=100, ssRtol=100,
+    if (missing(ssRtol)) {
+      ssRtol <- 100
+      .minfo(paste0("Since Monolix doesn't use ssRtol, set ssRtol=", ssRtol))
+    }
+    if (missing(ssAtol)) {
+      ssAtol <- 100
+      .minfo(paste0("Since Monolix doesn't use ssRtol, set ssAtol=", ssAtol))
+    }
+    .nss <- .getNbdoses(object)
+    if (missing(maxSS) && missing(maxSS)) {
+      maxSS <- .nss + 1
+      minSS <- .nss
+      .minfo(paste0("Since Monolix uses a set number of doses for steady state use maxSS=", maxSS,
+                    ", minSS=", minSS))
+    }
+    .cls <- class(object)
+    class(object) <- .cls[-which(.cls == "monolix2rx")]
+  })
+  .extra <- vapply(.extra,
+                   function(l) {
+                     if (identical(l, quote(`{`))) {
+                       return("")
+                     }
+                     return(paste(deparse(l), collapse="\n"))
+                   }, character(1), USE.NAMES=FALSE)[-1]
+  .args <- c(.args, "{", .extra)
+  .formalArgs <- as.character(eval(str2lang(paste0("formalArgs(rxode2::rxSolve)"))))
+  .w <- which(.formalArgs == "...")
+  .formalArgs <- paste0(.formalArgs, "=", .formalArgs)
+  .has3 <- FALSE
+  if (length(.w) > 0) {
+    .formalArgs[.w] <- "..."
+    .has3 <- TRUE
+  }
+  .formalArgs <- paste(.formalArgs, collapse=", ")
+  .formalArgs <- paste0("rxode2::rxSolve(", .formalArgs, ")")
+  .args <- c(.args, .formalArgs, "}")
+  .args <- paste(.args, collapse="\n")
+  .args <- c("# This is built from buildParser.R, edit there",
+             "#'@export", deparse(str2lang(.args)))
+  writeLines(.args, devtools::package_file("R/rxSolve.R"))
+  message("done")
+}
+
+.monolix2rxRxUiGetMethods <- function() {
+  message("build rxUiGet options to allow str() and dollar completion")
+  .meth <- c("nonmemData"="NONMEM input data from nonmem2rx",
+             "etaData"="NONMEM etas input from nonmem2rx",
+             "ipredAtol"="50th percentile of the IPRED atol comparison between rxode2 and model import",
+             "ipredRtol"="50th percentile of the IPRED rtol comparison between rxode2 and model import",
+             "ipredCompare"="Dataset comparing ID, TIME and the IPREDs between rxode2 and model import",
+             "predAtol"="50th percentile of the PRED atol comparison between rxode2 and model import",
+             "predRtol"="50th percentile of the PRED rtol comparison between rxode2 and model import",
+             "predCompare"="Dataset comparing ID, TIME and the PREDs between rxode2 and model import",
+             "sigma"="sigma matrix from model import",
+             "thetaMat"="covariance matrix",
+             "dfSub"="Number of subjects",
+             "dfObs"="Number of observations",
+             "atol"="atol imported from translation",
+             "rtol"="rtol imported from translation",
+             "ssRtol"="ssRtol imported from translation",
+             "ssAtol"="ssRtol imported from translation")
+  .ret <- paste(c("## nocov start",
+                  "# This is built from buildParser.R, edit there",
+                  vapply(seq_along(.meth), function(i) {
+                    .name <- names(.meth)[i]
+                    .desc <- setNames(.meth[i], NULL)
+                    .ret <- c("",
+                              sprintf("rxUiGet.%s <- function(x, ...) {", .name),
+                              "  .meta <- new.env(parent=emptyenv())",
+                              "  if (exists(\"meta\", envir=x[[1]])) .meta <- get(\"meta\", envir=x[[1]])",
+                              sprintf("  if (exists(\"%s\", envir=.meta)) return(get(\"%s\", envir=.meta))", .name, .name),
+                              sprintf("  if (!exists(\"%s\", envir=x[[1]])) return(NULL)", .name),
+                              sprintf("  get(\"%s\", envir=x[[1]])", .name),
+                              "}",
+                              sprintf("attr(rxUiGet.%s, \"desc\") <- %s", .name, deparse1(.desc)))
+                    .ret <- paste(.ret, collapse="\n")
+                  }, character(1), USE.NAMES=TRUE),
+                  ".rxUiGetRegister <- function() {",
+                  vapply(seq_along(.meth), function(i) {
+                    .name <- names(.meth)[i]
+                    sprintf("  rxode2::.s3register(\"rxode2::rxUiGet\", \"%s\")", .name)
+                  }, character(1), USE.NAMES=TRUE),
+                  "}",
+                  "## nocov end"), collapse="\n")
+  writeLines(.ret, devtools::package_file("R/rxUiGetGen.R"))
+  message("done")
+}
+
 ## nocov end
