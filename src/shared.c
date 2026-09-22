aa6f69f3083a260ad7479df211c7b000a84ab130
mattfidler <matthew.fidler@gmail.com>
2026-09-22 16:03:08 -0500

    style: explicitly NULL _dupStrs after free

--- src/shared.c
+++ src/shared.c
@@ -50,7 +50,8 @@ static int _dupStrsN = 0, _dupStrsMax = 0;
 
 void monolix2rx_dupStrsFree(void) {
   for (int i = 0; i < _dupStrsN; i++) R_Free(_dupStrs[i]);
-  if (_dupStrs != NULL) R_Free(_dupStrs); // R_Free() also sets _dupStrs = NULL
+  if (_dupStrs != NULL) R_Free(_dupStrs);
+  _dupStrs = NULL; // R_Free() already does this; explicit for clarity
   _dupStrsN = _dupStrsMax = 0;
 }
 
